6c4f83463b5ab50ff78e9e077db675769d6105f4
Rafael Moreira Cavalcante de Souza <125597354+RMCSa@users.noreply.github.com>
2023-10-04 15:11:45 -0300

    Update createSql.sql
    Signed-off-by: Rafael Moreira Cavalcante de Souza <125597354+RMCSa@users.noreply.github.com>

--- database/createSql.sql
+++ database/createSql.sql
@@ -6,9 +6,12 @@ CREATE TABLE taskFlow.users (
   user_id INT PRIMARY KEY NOT NULL,     -- Identificador único do usuário
   first_name VARCHAR(50) NOT NULL,      -- Nome do usuário (não pode ser nulo)
   last_name VARCHAR(50) NOT NULL,       -- Sobrenome do usuário (não pode ser nulo)
+  username VARCHAR(50) NOT NULL,   
   email VARCHAR(100) NOT NULL,          -- Endereço de email do usuário (não pode ser nulo)
   password_hash CHAR(64) NOT NULL,     -- Hash da senha do usuário (não pode ser nulo)
-  created_at DATETIME NOT NULL         -- Data e hora de criação do usuário (não pode ser nulo)
+  created_at DATETIME NOT NULL,         -- Data e hora de criação do usuário (não pode ser nulo)
+  updated_at DATETIME NOT NULL,
+  profle_image IMAGE
 );
 
 -- Tabela de informações de categorias
@@ -27,9 +30,10 @@ CREATE TABLE taskFlow.task (
   description VARCHAR(MAX),            -- Descrição da tarefa
   due_date DATETIME2 NOT NULL,         -- Data e hora de vencimento da tarefa (não pode ser nulo)
   created_at DATETIME2 NOT NULL,       -- Data e hora de criação da tarefa (não pode ser nulo)
+  updated_at DATETIME NOT NULL,
   completed BIT NOT NULL,             -- Indicador de tarefa concluída (não pode ser nulo)
   priority TINYINT NOT NULL,          -- Prioridade da tarefa (não pode ser nulo)
-  category_id INT,           -- ID da categoria à qual a tarefa pertence (pode ser nulo)
+  category_id INT,                    -- ID da categoria à qual a tarefa pertence (pode ser nulo)
   FOREIGN KEY (user_id) REFERENCES taskFlow.users(user_id),       -- Chave estrangeira para o usuário
   FOREIGN KEY (category_id) REFERENCES taskFlow.category(category_id) -- Chave estrangeira para a categoria
 );
